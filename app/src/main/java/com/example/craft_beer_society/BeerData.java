26de04a085d083779f7178718c62316f75a229b6
Majkl <majkl.andracic@gmail.com>
2020-02-16 22:41:28 +0100

    BeerData class

---
+++ app/src/main/java/com/example/craft_beer_society/BeerData.java
@@ -0,0 +1,13 @@
+package com.example.craft_beer_society;
+
+class BeerData {
+    String BeerName;
+    String Link;
+    String Image;
+
+    public BeerData(String beerName, String link, String image) {
+        this.BeerName = beerName;
+        this.Link = link;
+        this.Image = image;
+    }
+}
--- app/src/main/java/com/example/craft_beer_society/MainActivity.java
+++ app/src/main/java/com/example/craft_beer_society/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.craft_beer_society;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ListView list = findViewById(R.id.list);
+        ArrayList<BeerData> arrayList = new ArrayList<BeerData>();
+        arrayList.add(new BeerData())
     }
 }
