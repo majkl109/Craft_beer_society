acf293e6d58e2cf15bf2b75b04220fa6f1a18a6b
Majkl <majkl.andracic@gmail.com>
2020-03-03 22:15:34 +0100

    Added SplashActivity

---
+++ app/src/main/java/com/example/craft_beer_society/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.craft_beer_society;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        },3000);
+
+    }
+
+
+}
